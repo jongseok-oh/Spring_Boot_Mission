337a54ad539d1f05651b7cbe789060b44b44d108
JongKing <hynb14@gmail.com>
2022-04-26 18:22:25 +0900

    naver login

--- week8/auth/src/main/java/dev/jongking/auth/config/WebSecurityConfig.java
+++ week8/auth/src/main/java/dev/jongking/auth/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package dev.jongking.auth.config;
 
 import dev.jongking.auth.infra.CustomUserDetailsService;
+import dev.jongking.auth.infra.NaverOAuth2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+    private final NaverOAuth2Service naverOAuth2Service;
 
     public WebSecurityConfig(
-            @Autowired CustomUserDetailsService customUserDetailsService
+            @Autowired CustomUserDetailsService customUserDetailsService,
+            @Autowired NaverOAuth2Service naverOAuth2Service
             ){
         this.userDetailsService = customUserDetailsService;
+        this.naverOAuth2Service =naverOAuth2Service;
     }
 
     @Override
@@ -54,6 +58,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/user/login")
                 .defaultSuccessUrl("/home")
                 .permitAll()
+            .and()
+                .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(this.naverOAuth2Service)
+                .and()
+                    .defaultSuccessUrl("/home")
             .and()
                 .logout()
                 .logoutUrl("/user/logout")
