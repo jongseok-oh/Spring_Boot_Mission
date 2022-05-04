c2fa63320759e19553c53f80db21775169a94a06
JongKing <hynb14@gmail.com>
2022-05-04 17:46:20 +0900

    ws chatting

---
+++ week9/web-socket-ex/src/main/java/dev/jongking/websocketex/WebSocketExApplication.java
@@ -0,0 +1,13 @@
+package dev.jongking.websocketex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketExApplication.class, args);
+	}
+
+}
---
+++ week9/web-socket-ex/src/main/java/dev/jongking/websocketex/controller/WebSocketMapping.java
@@ -0,0 +1,31 @@
+package dev.jongking.websocketex.controller;
+
+import dev.jongking.websocketex.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class WebSocketMapping {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketMapping.class);
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public WebSocketMapping(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/ws/chat")
+    public void sendChat(ChatMessage chatMessage){
+        logger.info(chatMessage.toString());
+        final String time = new SimpleDateFormat("HH:mm").format(new Date());
+        simpMessagingTemplate.convertAndSend(
+                String.format("receive-endpoint/%s", chatMessage.getRoomId()),
+                chatMessage
+        );
+    }
+}
